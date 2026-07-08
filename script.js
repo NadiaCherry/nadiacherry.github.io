621494ec576a584a7f5870fff78d1324505e5985
NadiaCherry <nlee070300@gmail.com>
2026-07-07 22:36:33 -0500

    dropdown for skills

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("JavaScript Connected!");
+
+const competencyCards = document.querySelectorAll(".competency-card");
+
+competencyCards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        card.classList.toggle("active");
+
+        const details = card.querySelector(".competency-details");
+
+        if (details.classList.contains("hidden")) {
+
+            details.classList.remove("hidden");
+
+        } else {
+
+            details.classList.add("hidden");
+
+        }
+
+    });
+
+});
